0952d6c1af7e39c815f1fdedbcc7ceea519bc33c
FunDMe365 <fundasmile365@gmail.com>
2025-08-07 08:29:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'https://fundasmile.netlify.app',
+  origin: 'https://fundasmile.net',
   methods: ['GET', 'POST'],
 }));
 
