3ade49625a316ee703fe542df5cd44cc08ab5b2f
Corey Shelowitz <fundasmile365@gmail.com>
2025-10-19 01:17:28 -0400

    added waitlist fix and sendgrid route for email

--- server.js
+++ server.js
@@ -265,7 +265,6 @@ app.post("/api/waitlist", async (req, res) => {
 
   if (!name || !email || !source || !reason) {
     return res.status(400).json({ success: false, message: "All fields required." });
-  }
 
   try {
     // 1️⃣ Save to Google Sheet
