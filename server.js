8d2a637da2650d48ce0e4033bf3d483757656552
Corey Shelowitz <corey@fundasmile.net>
2025-12-23 06:27:55 -0500

    Fix connect-mongo import for v6

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // ==================== PRODUCTION-READY SESSION ====================
-const MongoStore = require("connect-mongo");
+const MongoStore = require("connect-mongo").default;
 
 app.set("trust proxy", 1);
 
